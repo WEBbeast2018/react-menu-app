fe48986bf2893f78c46f2fb1b6d4b110a56a8bc7
yuvalbl <yuval.bar.levi@gmail.com>
2018-06-10 20:53:43 +0300

    basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+const MenuItem = () => (
+  <li>Menu item
+  </li>
+);
+
+
+const AddMenuItem = () => (
+  <div>
+    <h2>Add New Item</h2>
+    <input/>
+    <button>Add</button>
+  </div>
+);
+
+const Menu = () => (
+  <div>
+    <h2>Menu</h2>
+    <ul>
+      <MenuItem/>
+      <MenuItem/>
+      <MenuItem/>
+      <MenuItem/>
+    </ul>
+  </div>
+);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddMenuItem/>
+        <Menu/>
       </div>
     );
   }
