d8689b3b5b1702df6e6279a99b3b542c081d137c
yuvalbl <yuval.bar.levi@gmail.com>
2018-06-13 15:31:49 +0300

    use es6 destructuring

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {MenuItem} from './MenuItem';
 
-export const Menu = (props) => (
+export const Menu = ({title, items}) => (
   <div>
-    <h2>{props.title}</h2>
+    <h2>{title}</h2>
     <ul>
-      <MenuItem item={props.items[0]}/>
-      <MenuItem item={props.items[1]}/>
-      <MenuItem item={props.items[2]}/>
-      <MenuItem item={props.items[3]}/>
+      <MenuItem item={items[0]}/>
+      <MenuItem item={items[1]}/>
+      <MenuItem item={items[2]}/>
+      <MenuItem item={items[3]}/>
     </ul>
   </div>
 );
